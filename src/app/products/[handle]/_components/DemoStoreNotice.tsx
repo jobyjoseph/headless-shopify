92e7e576965870b4076049a9189f52005a4715a2
Joby Joseph <reachjoby@gmail.com>
2026-03-09 11:37:42 -0700

    fix: update DemoStoreNotice link and text to reflect correct store information

--- src/app/products/[handle]/_components/DemoStoreNotice.tsx
+++ src/app/products/[handle]/_components/DemoStoreNotice.tsx
@@ -3,12 +3,12 @@ export function DemoStoreNotice() {
     <div className="my-4 text-lg font-light text-gray-700">
       This is a demonstration store. You can purchase products like this from{" "}
       <a
-        href="https://alo.com"
+        href="https://kotn.com/?utm_source=joby.blog&utm_medium=headless-demo"
         target="_blank"
         rel="noopener"
         className="font-normal hover:underline text-black"
       >
-        alo
+        Kotn
       </a>
       .
     </div>
